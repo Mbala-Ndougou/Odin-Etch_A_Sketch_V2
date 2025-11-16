8d78e87d72dd41bba7d6cf8b0c08cdf62cd5e9d4
mbalandougou <mbalandougou@gmail.com>
2025-11-16 11:55:31 +0100

    Complete eth a sketch App
    Finished version of the app. Working perfectly

--- etchASketch.js
+++ etchASketch.js
@@ -4,23 +4,48 @@ let containerCol = parseFloat(getComputedStyle(container).height);
 console.log(containerCol);
 console.log(containerRow);
 
-let n = Number(prompt('How many squares per line/column?'));
-console.log(n);
+
+function etchASketch() {
 
 
-for(let i = 0; i < (n*n); i++) {
-    
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    div.style.width = `${(containerCol/n)-1}px`;
-    div.style.height = `${(containerRow/n) -1}px`;  
-    console.log(div.style.width);
-    console.log(div.style.height);
-    
-    
-    container.appendChild(div);
 
-    div.textContent = "**"
+    let n;
     
-}   
+    do {
+        n = Number(prompt('Enter a number superior or equal to 1 or inferior or equal to 100'))
+    } while(n<1 || n > 100)
 
+    for(let i = 0; i < (n*n); i++) {
+        
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = `${(containerCol/n)-1}px`;
+        div.style.height = `${(containerRow/n) -1}px`;  
+        console.log(div.style.width);
+        console.log(div.style.height);
+
+        div.addEventListener('mouseover', e => e.target.style.backgroundColor = 'grey')
+        
+        
+        container.appendChild(div);
+
+        let reset = document.querySelector('.reset');
+        reset.addEventListener('click',() => {
+
+            let grids = document.querySelectorAll('.grid');
+            grids.forEach(div => {
+                div.style.backgroundColor = "rgb(243, 243, 243)";
+            });
+
+        })
+
+    }   
+
+
+}
+
+let start  = document.querySelector('.start');
+start.addEventListener('click', () => {
+    container.innerHTML = "";
+    etchASketch();    
+})
